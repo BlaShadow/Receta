aa69880360ca4538549527e4398ed14d2d4d3f4a
Norman Fernandez <el.norman01@gmail.com>
2015-03-11 05:10:48 +0000

    Recipe Support -Recipes can now be added. -Recipes will be shown on the list.

--- www/js/index.js
+++ www/js/index.js
@@ -1,81 +1,3 @@
-var Model = function(){
-    var databaseName = "recetas_db";
-    
-    /** save object to database **/
-    function saveData(datastorage){
-        localStorage.setItem(databaseName,JSON.stringify(data));    
-    }
-    
-    /** load current object **/
-    function loadData(){
-        var rawData = localStorage.getItem(databaseName);   
-        
-        var cleanData = JSON.parse(rawData);
-        
-        return cleanData;
-    }
-    
-    /** return all chef **/
-    function listChef(){
-        var item = loadData();
-        
-        if(item !== undefined){
-            return item.chefs;    
-        }
-        
-        return [];
-    }
-    
-    function addReceta(nombre,chefId,url){
-        var newItem = {
-            id:guid(),
-            nombre:nombre,
-            chef_id:chefId,
-            image:url
-        };
-        
-        wrapSaveitem(newItem,"recetas");
-        
-        console.log("Receta guardada");
-    }
-    
-    function addChef(nombre){
-        var newItem = {id:guid(),nombre:nombre};
-        
-        wrapSaveitem(newItem,"chefs");
-    }
-    
-    /** Private method **/
-    function wrapSaveitem(newItem,property){
-        var currentDataBase = loadData();
-        
-        /** if the database is null, make an init :) **/
-        if(currentDataBase === undefined || currentDataBase === null){
-            saveData(data);
-            
-            currentDataBase = loadData();
-        }
-        
-        if(currentDataBase[property] !== undefined && typeof([]) === currentDataBase[property]){
-            currentDataBase[property].push(newItem);
-        }else{
-            currentDataBase[property] = [newItem];
-        }
-        
-        /** Save Data **/
-        saveData(currentDataBase);
-    }
-    
-    
-    
-        return {
-        addReceta:addReceta,
-        addChef:addChef,
-        listChef:listChef,
-    };
-};
-
-
 /*  Model
     
         chef:{
@@ -87,16 +9,10 @@ var Model = function(){
             nombre
             chef_id
             imagen
+            rating
         }
     
-    */
-    
-    /** model **/
-    var data = {
-        chefs:[],
-        recetas:[]
-    };
-
+*/
     function initializeStorage(){
         /*Called to check localStorage at the begining*/
         
@@ -109,7 +25,46 @@ var Model = function(){
             localStorage.recipe = '[]';
     }
     
+    function saveRecipe(recipeName, imageURL,ratingDropDown,chefDropDown){
+        if(localStorage.chef === null || localStorage.chef === undefined){
+            alert('No hay chefs en la base de datos!');
+            return;
+        }
+        
+        if(recipeName.value === '' || imageURL.value === '')
+        {
+            alert('Existen campos vacíos!');
+            return;
+        }
+        
+        var recipe = {
+            'name': recipeName.value,
+            'image': imageURL.value,
+            'rating': ratingDropDown[ratingDropDown.selectedIndex].value,
+            'chefId': chefDropDown[chefDropDown.selectedIndex].value,
+            'chefName': chefDropDown[chefDropDown.selectedIndex].text
+        };
+        
+        var tempRecipe = JSON.parse(localStorage.recipe);
+        tempRecipe.push(recipe);
+        
+        localStorage.recipe = JSON.stringify(tempRecipe);
+        alert('Agregando receta');
+        
+        recipeName.value = '';
+        imageURL.value = '';
+        
+        loadRecipeList(
+            document.getElementById('recipeTable')
+        );
+        
+    }
+    
     function saveChef(chefName){
+        if(chefName.value === ''){
+            alert('Nombre del chef vacío!');
+            return;
+        }
         var chef = {
           'id':guid(),
           'name': chefName.value
@@ -119,41 +74,74 @@ var Model = function(){
         tempChef.push(chef);
         
         localStorage.chef = JSON.stringify(tempChef);
+        
+        alert('Guardando: ' + chef.name);
+        
+        chefName.value = '';
+        
+        loadChefList(
+            document.getElementById('chefTable'),
+            document.getElementById('chefDropDown')
+        );
+    }
+    
+    function loadRecipeList(recipeTable){
+        var i; //counter
+        var count = JSON.parse(localStorage.recipe).length; //no. of chefs
+        var recipeArray = JSON.parse(localStorage.recipe); //Array of chefs
+        var recipeList = '';
+        for(i = 0; i < count; i++){
+            recipeList += 
+                '<tr>'+ 
+                
+                    '<td>' + 
+                        recipeArray[i].name + 
+                    '</td>' +
+                
+                    '<td>' +
+                        recipeArray[i].rating + 
+                    '</td>'+ 
+                
+                    '<td>' +
+                        recipeArray[i].chefName + 
+                    '</td>'+
+                    
+                    '<td>' +
+                        '<a href="' + recipeArray[i].image + '">' +
+                            recipeArray[i].image + 
+                        '</a>' +
+                    '</td>'+
+                
+                '</tr>';
+        }
+        
+        /* Adding the HTML code */
+        recipeTable.innerHTML = recipeList;
     }
     
     function loadChefList(chefTable, chefDropDown){
-        var i;
-        var count = JSON.parse(localStorage.chef).length;
-        var chefArray = JSON.parse(localStorage.chef);
+        var i; //counter
+        var count = JSON.parse(localStorage.chef).length; //no. of chefs
+        var chefArray = JSON.parse(localStorage.chef); //Array of chefs
         var chefList = '';
         var chefScroll = '';
         for(i = 0; i < count; i++){
             chefList += '<tr><td>' + chefArray[i].id + '</td><td>' + chefArray[i].name + '</td></tr>';
             chefScroll += '<option value="' + chefArray[i].id +'">' + chefArray[i].name + '</option>';
         }
+        
+        /* Adding the HTML code */
         chefTable.innerHTML = chefList;
         chefDropDown.innerHTML = chefScroll;
     }
 
 /** generate guid (unique identifier) **/
     function guid() {
-      function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-          .toString(16)
-          .substring(1);
-      }
-      return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
         s4() + '-' + s4() + s4() + s4();
-    }  
-
-
-/*
-    Forma de uso:
-
-    var model = new Model();
-
-    model.listChef();
-    model.addChef("nombre");
-    model.addReceta("nombre","1231232342345","url");ß
-
-*/
+    }
